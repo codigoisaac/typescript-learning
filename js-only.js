32b62ce1bb624aa4a321c1572d25f5a58e750073
Isaac Muniz <isaacmuniz@protonmail.com>
2022-09-08 20:11:08 -0300

    Basic inputs and add function

---
+++ js-only.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('addButton')
+const input1 = document.getElementById('num1')
+const input2 = document.getElementById('num2')
+
+function add(a, b) {
+  return a + b
+}
+
+button.addEventListener('click', () => {
+  console.log(add(input1.value, input2.value))
+})
