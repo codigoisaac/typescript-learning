69cf0b6aeb7c407a1735f15ef91834ab4d1b3ac4
Isaac Muniz <isaacmuniz@protonmail.com>
2022-09-09 00:32:18 -0300

    Return values

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-function combine(a, b, resultConversion) {
-    var result;
-    if ((typeof a === 'number' && typeof b === 'number') ||
-        resultConversion === 'as-number') {
-        result = +a + +b;
-    }
-    else {
-        result = a.toString() + b.toString();
-    }
-    return result;
+function add(a, b) {
+    return a + b;
 }
-console.log(combine('this', 2, 'as-number'));
-console.log(combine('this', ' is not it', 'as-text'));
-console.log(combine(2, 7, 'as-text'));
-console.log(combine('2', '7', 'as-number'));
+function printResult(num) {
+    console.log('Result: ', num);
+}
+console.log(printResult(7));
