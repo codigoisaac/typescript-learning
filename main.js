ba428cedd7ac32450446e26837f10e85bd982242
Isaac Muniz <isaacmuniz@protonmail.com>
2022-09-08 23:30:29 -0300

    Tuples! (and mocking them)

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-function add(a, b, showResult, phrase) {
-    var result = a + b;
-    if (showResult)
-        console.log("".concat(phrase, " ").concat(result));
-    return result;
-}
-var a = 1, b = 2.7, result = add(a, b, true, 'Result is:');
+var person = {
+    name: 'Isaac',
+    age: 24,
+    hobbies: ['bass guitar', 'meditation'],
+    role: [2, 'Chief Technology Officer']
+};
+person.role.push('Visionaire'); // pushed type will not be catched
+// person.role[1] = 10; // assignment type will be catched
+var colors = ['blue', 'green', 'red'];
+console.log(person);
