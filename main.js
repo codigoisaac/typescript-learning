1f3979d7fd417299f1fc416b64a39821a13ca996
Isaac Muniz <isaacmuniz@protonmail.com>
2022-09-08 22:43:36 -0300

    One more ts example

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Hello, TypeScript!');
+function add(a, b, showResult, phrase) {
+    var result = a + b;
+    if (showResult)
+        console.log("".concat(phrase, " ").concat(result));
+    return result;
+}
+var a = 1, b = 2.7, result = add(a, b, true, 'Result is:');
--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-console.log('Hello, TypeScript!');
+function add(a: number, b: number, showResult: boolean, phrase: string) {
+  const result = a + b;
+
+  if (showResult) console.log(`${phrase} ${result}`);
+
+  return result;
+}
+
+const a = 1,
+  b = 2.7,
+  result = add(a, b, true, 'Result is:');
