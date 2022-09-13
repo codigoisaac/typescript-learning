562179d725f7443d8820f35b3f48c4ee5f00e4dd
Isaac Muniz <isaacmuniz@protonmail.com>
2022-09-12 22:05:45 -0300

    Function types

--- main.ts
+++ main.ts
@@ -2,9 +2,28 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
-let myValue: Function;
+type strN = string | number;
+
+function whatever(a: strN, b: strN, c: strN) {
+  console.log('whatever', a, b, c);
+}
+
+let myValue: (a: number, b: number) => number; //* Function type
 
 myValue = add;
 // myValue = 8; // TS shows error
 
 console.log(myValue(5, 7));
+
+//---
+
+//* Function type again
+
+function addAndHandle(a: number, b: number, callback: (sum: number) => void) {
+  const result = a + b;
+  return callback(result);
+}
+
+addAndHandle(1, 2, (result) => {
+  console.log(`Handled result is ${result}`);
+});
