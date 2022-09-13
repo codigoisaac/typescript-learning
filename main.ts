77f3740945f29abd31ceb8832aab13bae52ed633
Isaac Muniz <isaacmuniz@protonmail.com>
2022-09-12 22:23:30 -0300

    The 'unknown' type'

--- main.ts
+++ main.ts
@@ -1,29 +1,18 @@
-function add(a: number, b: number): number {
-  return a + b;
+//* 'unknow' is better than 'any' cause it involves the value being checked
+
+let userInput: unknown;
+// let userInput: any;
+
+let userName: string;
+
+userInput = 'ok';
+userInput = 7;
+
+// TS will show error - unknown is not garanteed to be a string
+// But if userInput is 'any', then it works
+// userName = userInput;
+
+// But this will work with userInput being 'unknown', because of the check
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-type strN = string | number;
-
-function whatever(a: strN, b: strN, c: strN) {
-  console.log('whatever', a, b, c);
-}
-
-let myValue: (a: number, b: number) => number; //* Function type
-
-myValue = add;
-// myValue = 8; // TS shows error
-
-console.log(myValue(5, 7));
-
-//---
-
-//* Function type again
-
-function addAndHandle(a: number, b: number, callback: (sum: number) => void) {
-  const result = a + b;
-  return callback(result);
-}
-
-addAndHandle(1, 2, (result) => {
-  console.log(`Handled result is ${result}`);
-});
